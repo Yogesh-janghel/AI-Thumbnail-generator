e4a4390031c0a3139e83521be191f7bf3d5a1e0a
Yogesh Janghel <yogeshjanghel819@gmail.com>
2026-02-10 02:40:54 +0530

    session and cookie property updated

--- server/server.ts
+++ server/server.ts
@@ -24,6 +24,9 @@ app.use(cors({
     credentials:true
 
 }))
+
+app.set('trust proxy', 1);
+
 app.use(session({
     secret: process.env.SESSION_SECRET as string,
     resave: false,
@@ -31,7 +34,7 @@ app.use(session({
     cookie: { maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'none',
+        sameSite: process.env.NODE_ENV === 'production'? 'none':'lax',
         path: '/'
      }, // 7 days in milliseconds
     store: MongoStore.create({
